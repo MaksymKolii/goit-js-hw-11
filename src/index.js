98666cde0ffa66cdd41e0818544261e9497d3a11
Maksym Kolii <maksymkolii@gmail.com>
2022-10-19 00:22:37 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,91 +1,144 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-import { galleryItems } from './gallery-items';
+import LoadMoreBtn from '../src/js-components/loadMoreBtn'
 
 import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { SerchImagesByKeyWord } from './searchApi';
+const URL = 'https://pixabay.com/api/';
+//const Filter = 'image_type=photo&orientation=horizontal&safesearch=true';
+ const axios = require('axios').default;
 
-// const API_KEY = '30620047-2b41fea3ffb04e82a67076d5b';
-// const url='https://pixabay.com/api/'
-// const URL = `${url}?key=${API_KEY}+"&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
-// user_id:30620047
-// Your API key: 30620047-2b41fea3ffb04e82a67076d5b
+//import axios from 'axios';
+// const axios = require('axios');
 
-// https://pixabay.com/api?key=30620047-2b41fea3ffb04e82a67076d5b&image_type=photo&orientation=horizontal&safesearch=true
-// const axios = require('axios').default;
-
-const refs = {
-  form: document.querySelector('#search-form'),
-  // input: document.querySelector('input[name="searchQuery"]'),
-  galleryElRef:document.querySelector(".gallery"),
-};
-
-refs.form.addEventListener('submit', onSubmit);
+const searchParams = new URLSearchParams({
+  key: '30620047-2b41fea3ffb04e82a67076d5b',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+});
 
 
-const serchImagesByKeyWord = new SerchImagesByKeyWord();
-
-function onSubmit(e) {
-
-  e.preventDefault()
-
-  serchImagesByKeyWord.word = e.currentTarget.elements.searchQuery.value;
-
-  console.log(e.currentTarget.elements.searchQuery.value);
-  if (serchImagesByKeyWord.word === '') return;
-
-  //serchImagesByKeyWorld.fetchImagesByKeyWord()
-
-  serchImagesByKeyWord.fetchImagesByKeyWord().then((artic)=> {
-    console.log(artic);
-   
-    makeGalleryMarkup(artic)})
-  // .then(data =>{
-
-  // //  refs.galleryElRef.insertAdjacentHTML("beforeend", makeGalleryMarkup(galleryItems));
-   
-
-  // })
-  // .catch(function (error) {
-  //   if (error.response) {
-  //     // Запрос был сделан, и сервер ответил кодом состояния, который
-  //     // выходит за пределы 2xx
-  //     console.log(error.response.data);
-  //     console.log(error.response.status);
-  //     console.log(error.response.headers);
-  //   } else if (error.request) {
-  //     // Запрос был сделан, но ответ не получен
-  //     // `error.request`- это экземпляр XMLHttpRequest в браузере и экземпляр
-  //     // http.ClientRequest в node.js
-  //     console.log(error.request);
-  //   } else {
-  //     // Произошло что-то при настройке запроса, вызвавшее ошибку
-  //     console.log('Error', error.message);
-  //   }
-  //   console.log(error.config);
-  // });
-
-
- // refs.galleryElRef.insertAdjacentHTML("beforeend", makeGalleryMarkup(galleryItems));
+    let keyWord = '';
+    let page = 1;
   
-}
 
+ function fetchImagesByKeyWord(keyWord, page) {
+    const search = `${URL}?q=${keyWord}&${searchParams}&page=${page}`;
 
-// function fetchArticles(){
-//   //loadMoreBtn.disable();
-//   serchImagesByKeyWord.fetchArticles().then((artic)=> {
-//     renderEvents(artic)
-//     //loadMoreBtn.enable();
-//   });
-// }
+    // return fetch(search).then(response => {
+    //   if (!response.ok) {
+    //     throw new Error(response.status);
+    //   }
+    //   return response.json();
+    // });
+
+   return axios.get(search)
+      .then(function (response) {
+        // handle success
+        //makeGalleryMarkup(response.data)
+        console.log(response.data);
+        //return response;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
+  fetchImagesByKeyWord()
 
 new SimpleLightbox(".gallery__link", {
   captionsData: "alt",
   captionDelay: 250,
 });
+// const API_KEY = '30620047-2b41fea3ffb04e82a67076d5b';
+// const url='https://pixabay.com/api/'
+// const URL = `${url}?key=${API_KEY}+"&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+// user_id:30620047
+// Your API key: 30620047-2b41fea3ffb04e82a67076d5b
+// https://pixabay.com/api?key=30620047-2b41fea3ffb04e82a67076d5b&image_type=photo&orientation=horizontal&safesearch=true
+// const axios = require('axios').default;
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  galleryElRef:document.querySelector(".gallery"),
+  loadMoreBtn: document.querySelector('.load-more')
+};
+
+refs.form.addEventListener('submit', onSubmit);
+
+// const loadMoreBtn = new LoadMoreBtn({
+//   selector: '[data-action="load-more"]',
+
+//   hidden: false,
+// });
+
+const serchImagesByKeyWord = new SerchImagesByKeyWord();
+
+// function onSubmit(e) {
+//   e.preventDefault()
+//   serchImagesByKeyWord.word = e.target.elements.searchQuery.value;
+
+//   if (serchImagesByKeyWord.word === '') return;
+
+
+//   serchImagesByKeyWord.fetchImagesByKeyWord().then((artic)=> {
+//     //console.log(artic);
+//     return artic
+   
+//     //makeGalleryMarkup(artic.hits)
+//   })
+
+
+
+//   // .catch(function (error) {
+//   //   if (error.response) {
+//   //     // Запрос был сделан, и сервер ответил кодом состояния, который
+//   //     // выходит за пределы 2xx
+//   //     console.log(error.response.data);
+//   //     console.log(error.response.status);
+//   //     console.log(error.response.headers);
+//   //   } else if (error.request) {
+//   //     // Запрос был сделан, но ответ не получен
+//   //     // `error.request`- это экземпляр XMLHttpRequest в браузере и экземпляр
+//   //     // http.ClientRequest в node.js
+//   //     console.log(error.request);
+//   //   } else {
+//   //     // Произошло что-то при настройке запроса, вызвавшее ошибку
+//   //     console.log('Error', error.message);
+//   //   }
+//   //   console.log(error.config);
+//   // });
+
+
+  
+// }
+
+function onSubmit(e) {
+  e.preventDefault()
+  keyWord = e.target.elements.searchQuery.value;
+
+  if (keyWord === '') return;
+
+
+  fetchImagesByKeyWord(keyWord,1).then((artic)=> {
+    //console.log(artic);
+    return artic
+   
+    //makeGalleryMarkup(artic.hits)
+  })
+
+
+  
+}
+
+
 
 function makeGalleryMarkup(arr) {
 
@@ -93,7 +146,7 @@ function makeGalleryMarkup(arr) {
       ({ webformatURL, largeImageURL , tags , likes , views , comments, downloads  }) => {return  `<li class="gallery__item">
               <a class="gallery__link" href="${largeImageURL}">
               <div class="photo-card">
-              <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+              <img src="${webformatURL}" alt="${tags}" loading="lazy" width='240px' hight ='400px' />
               <div class="info">
                 <p class="info-item">
                   <b>Likes${likes}</b>
@@ -119,107 +172,91 @@ function makeGalleryMarkup(arr) {
 }
 
 
-// function makeGalleryMarkup(object) {
-//   return object
-//     .map(
-//       ({ original, preview, description }) => `<li class="gallery__item">
-//               <a class="gallery__link" href="${original}">
-//                    <img
-//                       class="gallery__image"
-//                       src="${preview}"
-//                       alt="${description}"
-//                   />
-//               </a>
-//           </li>`
-//     )
+
+
+
+
+
+
+
+// const form = document.querySelector("form");
+// const list = document.querySelector(".list");
+// const button = document.querySelector(".more");
+
+// const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
+// const API = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
+
+// let pageToFetch = 0;
+// let keyword = "";
+
+// function fetchEvent(page, keyword) {
+//   const params = new URLSearchParams({
+//     apikey: API,
+//     page,
+//     keyword,
+//     size: 50,
+//   });
+
+//   return fetch(`${BASE_URL}?${params}`)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .catch((error) => console.log(error));
+// }
+
+// function getEvents(page, keyword) {
+//   fetchEvent(page, keyword).then((data) => {
+//     console.log(data.page.totalElements);
+
+//     if (data.page.totalElements === 0) {
+//       button.classList.add("invisible");
+//       alert(`There are no events by keyword ${keyword}`);
+//     }
+
+//     const events = data?._embedded?.events;
+//     if (events) {
+//       renderEvents(events);
+//     }
+
+//     if (pageToFetch === data.page.totalPages - 1) {
+//       button.classList.add("invisible");
+//       alert("Finish");
+//       return;
+//     }
+//     pageToFetch += 1;
+//     if (data.page.totalPages > 1) {
+//       button.classList.remove("invisible");
+//     }
+//   });
+// }
+
+// function renderEvents(events) {
+//   const markup = events
+//     .map(({ name, images }) => {
+//       return `<li>
+//     <img src='${images[0].url}' alt='${name}' width='200'>
+//     <p>${name}</p>
+//     </li>`;
+//     })
 //     .join("");
-// 
+//   list.insertAdjacentHTML("beforeend", markup);
+// }
 
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   const query = event.target.elements.query.value;
+//   keyword = query;
+//   pageToFetch = 0;
+//   list.innerHTML = "";
+//   if (!query) {
+//     return;
+//   }
+//   getEvents(pageToFetch, query);
+// });
 
-
-
-
-
-
-
-const form = document.querySelector("form");
-const list = document.querySelector(".list");
-const button = document.querySelector(".more");
-
-const BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
-const API = "9cTjAjlRB53wyhAFk5VzXcBu5GiPU6fK";
-
-let pageToFetch = 0;
-let keyword = "";
-
-function fetchEvent(page, keyword) {
-  const params = new URLSearchParams({
-    apikey: API,
-    page,
-    keyword,
-    size: 50,
-  });
-
-  return fetch(`${BASE_URL}?${params}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .catch((error) => console.log(error));
-}
-
-function getEvents(page, keyword) {
-  fetchEvent(page, keyword).then((data) => {
-    console.log(data.page.totalElements);
-
-    if (data.page.totalElements === 0) {
-      button.classList.add("invisible");
-      alert(`There are no events by keyword ${keyword}`);
-    }
-
-    const events = data?._embedded?.events;
-    if (events) {
-      renderEvents(events);
-    }
-
-    if (pageToFetch === data.page.totalPages - 1) {
-      button.classList.add("invisible");
-      alert("Finish");
-      return;
-    }
-    pageToFetch += 1;
-    if (data.page.totalPages > 1) {
-      button.classList.remove("invisible");
-    }
-  });
-}
-
-function renderEvents(events) {
-  const markup = events
-    .map(({ name, images }) => {
-      return `<li>
-    <img src='${images[0].url}' alt='${name}' width='200'>
-    <p>${name}</p>
-    </li>`;
-    })
-    .join("");
-  list.insertAdjacentHTML("beforeend", markup);
-}
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const query = event.target.elements.query.value;
-  keyword = query;
-  pageToFetch = 0;
-  list.innerHTML = "";
-  if (!query) {
-    return;
-  }
-  getEvents(pageToFetch, query);
-});
-
-button.addEventListener("click", () => {
-  getEvents(pageToFetch, keyword);
-});
+// button.addEventListener("click", () => {
+//   getEvents(pageToFetch, keyword);
+// });
--- src/searchApi.js
+++ src/searchApi.js
@@ -1,16 +1,18 @@
 //const API_KEY = '30620047-2b41fea3ffb04e82a67076d5b';
 const URL = 'https://pixabay.com/api/';
 //const Filter = 'image_type=photo&orientation=horizontal&safesearch=true';
-const axios = require('axios').default;
+ const axios = require('axios').default;
+
+//import axios from 'axios';
+// const axios = require('axios');
 
 const searchParams = new URLSearchParams({
-  key:'30620047-2b41fea3ffb04e82a67076d5b',
-  image_type:'photo',
-  orientation:'horizontal',
-  safesearch:true,
-  per_page: 40
+  key: '30620047-2b41fea3ffb04e82a67076d5b',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
 });
-
 
 export class SerchImagesByKeyWord {
   constructor() {
@@ -18,36 +20,41 @@ export class SerchImagesByKeyWord {
     this.page = 1;
   }
 
-
-  fetchImagesByKeyWord(){
-    // const searchAddress = `${URL}?key=${API_KEY}&q=${this.keyWord}&${Filter}&page=${this.page}&per_page=40`;
+  fetchImagesByKeyWord() {
     const search = `${URL}?q=${this.keyWord}&${searchParams}&page=${this.page}`;
-   
-    return fetch(search).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
+
+    // return fetch(search).then(response => {
+    //   if (!response.ok) {
+    //     throw new Error(response.status);
+    //   }
+    //   return response.json();
+    // });
+
+   return axios.get(search)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        //return response;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
   }
 
-  async getImagesByKeyWord() {
-    const search = `${URL}?q=${this.keyWord}&${searchParams}&page=${this.page}`;
-    try {
-      // const response = await axios.get('/user?ID=30620047');
-      const response = await axios.get(search);
-      console.log(response);
-      // return response.json();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  // axios Асинхронная
-  // fetchImagesByKeyWord() {
-  //   const searchAddress = `${URL}?key=${API_KEY}&q=${this.keyWord}&${Filter}&page=${this.page}&per_page=40`;
-
-  //   return this.getUser(searchAddress);
+  // async getImagesByKeyWord() {
+  //   const search = `${URL}?q=${this.keyWord}&${searchParams}&page=${this.page}`;
+  //   try {
+  //     // const response = await axios.get('/user?ID=30620047');
+  //     const response = await axios.get(search);
+  //     console.log(response);
+  //     // return response.json();
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
   // }
 
   set word(newWord) {
@@ -57,33 +64,3 @@ export class SerchImagesByKeyWord {
     return this.keyWord;
   }
 }
-
-/*// Обычный fetch
-  // fetchImagesByKeyWorld(){
-  //   const searchAddress = `${URL}?key=${API_KEY}&q=${this.keyWorld}&${Filter}&page=${this.page}&per_page=40`;
-  //   //const url =`https://pixabay.com/api?key=30620047-2b41fea3ffb04e82a67076d5b&q=${this.keyWorld}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
-  //   return fetch(searchAddress).then(response => {
-  //     if (!response.ok) {
-  //       throw new Error(response.status);
-  //     }
-  //     return response.json();
-  //   });
-  // }
-
-  // axios не Асинхронная
-  // fetchImagesByKeyWorld() {
-  //   const searchAddress = `${URL}?key=${API_KEY}&q=${this.keyWorld}&${Filter}&page=${this.page}&per_page=40`;
-  //   return axios
-  //     .get(searchAddress)
-  //     .then(function (response) {
-  //       // обработка успешного запроса
-  //       console.log(response);
-  //     })
-  //     .catch(function (error) {
-  //       // обработка ошибки
-  //       console.log(error);
-  //     })
-  //     .then(function () {
-  //       // выполняется всегда
-  //     });
-  // } */
